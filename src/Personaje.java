68c1383666fb1d235f7907f44944d7f4f0081b0c
Oliver <oliverr@ciencias.unam.mx>
2026-02-26 10:12:38 -0600

    Modifiqué Personaje para que fuera comprensible

--- src/Personaje.java
+++ src/Personaje.java
@@ -1,4 +1,40 @@
+import java.util.List;
+import java.util.Random;
 
 public abstract class Personaje {
-        
+        protected String nombre;
+    protected int puntosVida;
+    protected ComportamientoHabilidad comportamiento;
+    protected CatalogoHabilidades catalogo;
+
+    public void recibirDanio(int danio){
+        int vidaResultante = this.puntosVida - danio;
+        this.puntosVida = vidaResultante > 0 ? vidaResultante : 0;
+    }
+
+    public void adquirirPoder(){
+        List<ComportamientoHabilidad> opciones = catalogo.getHabilidades();
+        this.comportamiento = opciones.get(new Random().nextInt(opciones.size()));
+    }
+  
+    public boolean estaVivo(){
+        return this.puntosVida > 0;
+    }
+
+    public ComportamientoHabilidad getComportamiento() {
+        return this.comportamiento;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public int getPuntosVida() {
+        return this.puntosVida;
+    }
+
+
+
+    public abstract String atacar();
+    public abstract String defender();
 }
