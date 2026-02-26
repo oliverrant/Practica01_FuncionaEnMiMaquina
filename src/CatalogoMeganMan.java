23e444879b05569b86c203d39f7534945232be9f
Valeria Ca <12345678+czvaleria@users.noreply.github.com>
2026-02-26 15:42:31 -0600

    Acualización de poderes

--- src/CatalogoMeganMan.java
+++ src/CatalogoMeganMan.java
@@ -4,7 +4,7 @@ public class CatalogoMeganMan implements CatalogoHabilidades {
     
     @Override
     public List<ComportamientoHabilidad> getHabilidades() {
-        return List.of(new ComportamientoElectrico(), new ComportamientoProyectil(), new ComportamientoDeslizante());
+        return List.of(new ComportamientoElectrico(), new ComportamientoProyectil(), new ComportamientoHipnotizar());
     }
 
 }
