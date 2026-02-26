86dd115747e2b9e2cbda39c8124f72d2e4d1de4a
Oliver <oliverr@ciencias.unam.mx>
2026-02-26 10:41:24 -0600

    Eliminé clases raras

--- src/EventoTipo.java
+++
@@ -1,8 +0,0 @@
-public enum EventoTipo {
-    INICIO_COMBATE,
-    TURNO,
-    ATAQUE,
-    CAMBIO_PODER,
-    MUERTE,
-    FIN_COMBATE
-}
--- src/ObserverCombate.java
+++
@@ -1,7 +0,0 @@
-/**
- * Interfaz Observer (patrón Observer).
- */
-public interface ObserverCombate {
-    void onEvento(EventoCombate evento);
-    void onFinCombate(Personaje ganador, Personaje favorito);
-}
