4f03b53c249ca3323d89d8baec7b23d9b84b8495
Valeria Ca <12345678+czvaleria@users.noreply.github.com>
2026-02-27 09:44:59 -0600

    Hice el javadoc

--- src/ComportamientoBase.java
+++ src/ComportamientoBase.java
@@ -1,20 +1,49 @@
+/**
+ * Implementación básica de una habilidad.
+ * 
+ * Representa el comportamiento por defecto de ataque y defensa
+ * de un personaje.
+ */
 public class ComportamientoBase implements ComportamientoHabilidad{
     
+     /**
+     * Genera el mensaje de un ataque básico.
+     *
+     * @param nombreAtacante nombre del personaje que ataca
+     * @param nombreObjetivo nombre del personaje que recibe el ataque
+     * @return mensaje descriptivo del ataque
+     */
     @Override
     public String atacar(String nombreAtacante, String nombreObjetivo) {
         return nombreAtacante + " lanza un ataque básico a " + nombreObjetivo;
     }
 
+    /**
+     * Genera el mensaje de una defensa básica.
+     *
+     * @param nombrePersonaje nombre del personaje que se defiende
+     * @return mensaje descriptivo de la defensa
+     */
     @Override
     public String defender(String nombrePersonaje) {
         return nombrePersonaje + " se defiende de forma básica";
     }
 
+    /**
+     * Devuelve el daño base de esta habilidad.
+     *
+     * @return valor de daño
+     */
     @Override
     public int getDanio() {
         return 2;
     }
 
+    /**
+     * Devuelve la defensa base de esta habilidad.
+     *
+     * @return valor de defensa
+     */
     @Override
     public int getDefensa() {
         return 1;
--- src/ComportamientoProyectil.java
+++ src/ComportamientoProyectil.java
@@ -1,20 +1,49 @@
+/**
+ * Implementación de una habilidad de tipo proyectil.
+ * 
+ * Representa un ataque lanzando un proyectil grande
+ * y una defensa basada en el movimiento constante.
+ */
 public class ComportamientoProyectil implements ComportamientoHabilidad {
     
+    /**
+     * Genera el mensaje de ataque con proyectil.
+     *
+     * @param nombreAtacante nombre del personaje que ataca
+     * @param nombreObjetivo nombre del personaje objetivo
+     * @return mensaje descriptivo del ataque
+     */
     @Override
     public String atacar(String nombreAtacante, String nombreObjetivo){
         return nombreAtacante + " le lanza un proyectil (grande) a " + nombreObjetivo;
     }
 
+    /**
+     * Genera el mensaje de defensa.
+     *
+     * @param nombrePersonaje nombre del personaje que se defiende
+     * @return mensaje descriptivo de la defensa
+     */
     @Override
     public String defender(String nombrePersonaje){
         return nombrePersonaje + " corre en círculos";
     }
     
+    /**
+     * Devuelve el daño de la habilidad.
+     *
+     * @return valor de daño
+     */
     @Override
     public int getDanio(){
         return 6;
     }
 
+    /**
+     * Devuelve el valor de defensa de la habilidad.
+     *
+     * @return valor de defensa
+     */
     @Override
     public int getDefensa(){
         return 1;
