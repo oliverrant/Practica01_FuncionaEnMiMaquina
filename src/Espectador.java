9fc3ecf4b5c7a8b68bff9aa39e22d16fd7a7b6a5
Valeria Ca <12345678+czvaleria@users.noreply.github.com>
2026-02-26 22:47:54 -0600

    Añadí la clase Espectador para Observer

---
+++ src/Espectador.java
@@ -0,0 +1,56 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Espectador implements Observador{
+    private int id;
+    private Personaje personaje;
+    private BufferedWriter bw;
+    private boolean combateTerminado;
+
+    public Espectador(int id, Personaje personaje){
+        this.id = id;
+        this.personaje = personaje;
+
+        try {
+            this.bw = new BufferedWriter(new FileWriter(id + "_bitacora.txt"));
+            } catch (IOException e) {
+                System.out.println("Error al crear la bitacora");
+            }
+    }
+
+    @Override public void recibirActualizacion(String actualizacion){
+        if (combateTerminado) return;
+        if(bw != null){
+            try{
+                bw.write(actualizacion);
+                bw.newLine();
+            // si 
+            if(actualizacion.contains("Ganador:")){
+                if(actualizacion.contains(personaje.getNombre())){
+                    bw.write("Tu personaje gano");
+                } else {
+                    bw.write("Tu personaje perdio");
+                }
+                bw.newLine();
+                combateTerminado = true;
+            }
+            bw.flush();
+        } catch (IOException e) {
+                System.out.println("Error al escribir en la bitacora");
+            }
+        }
+    }
+
+    public void cerrarBitacora(){
+        if(bw != null){
+            try {
+                bw.close();
+            } catch (Exception e) {
+                System.out.println("Error al cerrar");
+            }
+            
+        }
+    }
+
+}
